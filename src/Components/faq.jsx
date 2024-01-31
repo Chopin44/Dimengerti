813d4ab811d7a8aec004e4add185877ea5c0054d
“Rodhi <ronaldorodhi@gmail.com>
2024-01-31 16:59:50 +0700

    butuh tambahan faq

--- src/Components/faq.jsx
+++ src/Components/faq.jsx
@@ -9,20 +9,20 @@ const FAQ = ({ faqs }) => {
   };
 
   return (
-    <div className="container mx-auto mt-8 p-4">
-      <h1 className="text-2xl font-bold mb-4">Frequently Asked Questions</h1>
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Frequently Asked Questions (pertanyaan yang sering ditanyakan)</h1>
       {faqs.map((faq, index) => (
         <div key={index} className="mb-4">
           <div
-            className="flex items-center justify-between p-4 bg-gray-200 cursor-pointer"
+            className="flex items-center justify-between p-4 bg-warna2 cursor-pointer"
             onClick={() => toggleFAQ(index)}
           >
-            <div className="font-semibold">{faq.question}</div>
+            <div className="font-semibold">{faq.pertanyaan}</div>
             <div className={`transition-transform ${openIndex === index ? 'transform rotate-180' : ''}`}>
               &#9660;
             </div>
           </div>
-          {openIndex === index && <div className="p-4 bg-white">{faq.answer}</div>}
+          {openIndex === index && <div className="p-4 bg-white">{faq.jawaban}</div>}
         </div>
       ))}
     </div>
---
+++ src/Components/requirement.jsx
@@ -0,0 +1,24 @@
+// src/components/Requirements.jsx
+import React from 'react';
+
+const Requirements = () => {
+  return (
+    <div className="font-IBM max-w-md mx-auto mt-8 bg-gray-200 p-4 rounded-md">
+      <h2 className="text-xl font-bold mb-4">Aplikasi dapat digunakan dengan minimal spesifikasi:</h2>
+
+      <div className="mb-2">
+        <strong>Perangkat:</strong> PC/Android/Ios
+      </div>
+
+      <div className="mb-2">
+        <strong>Kamera : </strong> 720 HD
+      </div>
+      
+      <div className="mb-2">
+        <strong>RAM : </strong> 4 GB
+      </div>
+    </div>
+  );
+};
+
+export default Requirements;
